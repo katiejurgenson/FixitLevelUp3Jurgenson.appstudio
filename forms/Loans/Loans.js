6ca3e9c37bbd0f7b9e7d9399b42fd3cc8a38ec58
Katie Jurgenson <katiejurgenson@users.noreply.github.com>
2020-09-28 20:03:38 -0500

    Adding / Naming Controls
    Setting up the framework for the inputs, button, and label. Setting starting values to variables.

---
+++ forms/Loans/Loans.js
@@ -0,0 +1,12 @@
+// Start here. 
+
+btnEnter.onclick=function(){
+  let principleAmt = Number(inptPrinciple.value)
+  let mnthInterest = Number(inptMnInterest.value)
+  let mnthTotal = Number(inptMonths.value)
+}
+
+
+
+// Label
+// lblPaymentMessage.value = `A TBD loan for ${principleAmt} over ${mnthTotal} at ${mnthInterest} interest would have a monthly payment of TBD.`
