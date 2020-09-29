2829c20defdfe4b7e39d9528af9621cf9364a326
Katie Jurgenson <katiejurgenson@users.noreply.github.com>
2020-09-29 12:41:19 -0500

    Created Start Form
    Wanted a Start form to choose between Car Loan or Home Loan and go to a new form accordingly

---
+++ forms/carLoan/carLoan.js
@@ -0,0 +1,28 @@
+// Start here. 
+
+/*
+
+btnEnter.onclick=function(){
+  let principleAmt = Number(inptPrinciple.value)
+  let mnthInterest = Number(inptMnInterest.value)
+  let mnthTotal = Number(inptMonths.value)
+}
+
+let userNum1 = Number(prompt("Enter a number:"));
+let userNum2 = Number(prompt("Enter a second number:")); 
+
+function carLoanPayment (principle, interest, months) {
+  return principle * (interest * ((1 + interest)**months)) / (((1 + interest)**months)-1)
+}
+
+let summedNumbers = calculate(userNum1, userNum2);
+
+function calculate (num1, num2) {
+  return num1 + num2; 
+}
+
+
+// Label
+// lblPaymentMessage.value = `A TBD loan for ${principleAmt} over ${mnthTotal} at ${mnthInterest} interest would have a monthly payment of TBD.`
+
+*/
